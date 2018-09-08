4ad8d4b63056f21de9a4de0f371df661c2ea2be1
Nathan Bardwell <nathan.bardwell96@gmail.com>
2018-09-08 01:20:01 -0400

    Added custom js file, added CSS

---
+++ src/main/webapp/js/custom-scripts.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let searchBy = document.querySelector('select[name="searchBy"]');
+searchBy.addEventListener('change', searchHandler);
+
+let ezConnect = document.getElemantById('ezConnect');
+ezConnect.addEventListener('change', installDateHandler)
+});
+
+function searchHandler(event) {
+    let value = event.currentTarget.value;
+    if (value === 'date_of_sale' || value === 'install_date') {
+        document.getElementById('date-search').classList.remove("d-none");
+        document.getElementById('text-search').classList.add("d-none");
+    } else {
+        document.getElementById('date-search').classList.add("d-none");
+        document.getElementById('text-search').classList.remove("d-none");
+    }
+}
+
+function installDateHandler(event) {
+    let value = event.currentTarget.value;
+    if (value === true) {
+
+    }
+}
